eb7f0cbecd44c85a1fd89e99e688df87dfd0c11e
Mario Schillermann <mario.schillermann@gmail.com>
2025-02-14 18:33:07 +0100

    draw tilde for rows

--- main.go
+++ main.go
@@ -8,9 +8,35 @@ import (
 	"github.com/schillermann/vimgo/terminal"
 )
 
-var terminalMode = terminal.TerminalMode{}
+var terminalWindow = terminal.TerminalWindow{}
 var reader = bufio.NewReader(os.Stdin)
 
+/*** output ***/
+func editorRefreshScreen() {
+	// clear screen
+	fmt.Fprint(os.Stdout, "\x1b[2J")
+	fmt.Fprint(os.Stdout, "\x1b[H")
+
+	editorDrawRows()
+
+	// reposition cursor
+	fmt.Fprint(os.Stdout, "\x1b[H")
+}
+
+func editorDrawRows() {
+	var _, rows, err = terminalWindow.NumberOfColumnsAndRows()
+	if err != nil {
+		exit(err)
+	}
+	for j := 0; j < rows; j++ {
+		fmt.Fprint(os.Stdout, "~")
+		if j < rows-1 {
+			fmt.Fprint(os.Stdout, "\r\n")
+		}
+	}
+}
+
+/*** Input ***/
 func keyPress() (rune, error) {
 	char, _, err := reader.ReadRune()
 	if err != nil {
@@ -19,28 +45,43 @@ func keyPress() (rune, error) {
 	return char, nil
 }
 
-func main() {
-	var err = terminalMode.EnableRawMode()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+func exit(withErr error) {
+	// clear screen and
+	fmt.Fprint(os.Stdout, "\x1b[2J")
+	// reposition cursor to home
+	fmt.Fprint(os.Stdout, "\x1b[H")
+
+	var disableRawModeErr = terminalWindow.DisableRawMode()
+	if disableRawModeErr != nil {
+		fmt.Fprintf(os.Stderr, "error disabling terminal raw mode: %v\n", disableRawModeErr)
 		os.Exit(1)
 	}
 
+	if withErr == nil {
+		os.Exit(0)
+	}
+
+	fmt.Fprint(os.Stderr, withErr)
+	os.Exit(1)
+}
+
+func main() {
+	var enableRawModeErr = terminalWindow.EnableRawMode()
+	if enableRawModeErr != nil {
+		exit(enableRawModeErr)
+	}
+
 	for {
+		editorRefreshScreen()
+
 		var keyPress, keyPressErr = keyPress()
 		if keyPressErr != nil {
-			fmt.Fprintln(os.Stderr, keyPressErr)
-			os.Exit(1)
+			exit(keyPressErr)
 		}
 
 		switch keyPress {
 		case 'q':
-			var disableRawModeErr = terminalMode.DisableRawMode()
-			if disableRawModeErr != nil {
-				fmt.Fprintf(os.Stderr, "error disabling terminal raw mode: %v\n", disableRawModeErr)
-				os.Exit(1)
-			}
-			os.Exit(0)
+			exit(nil)
 		default:
 			fmt.Println(keyPress)
 		}
