813f0f8ec056ddccd5ee7ec1b01d3a0c4e14e510
Mario Schillermann <mario.schillermann@gmail.com>
2025-02-14 18:31:37 +0100

    get number of columns and rows in terminal

--- terminal/terminal_window_unix.go
+++ terminal/terminal_window_unix.go
@@ -8,12 +8,21 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-type TerminalMode struct {
+type TerminalWindow struct {
 	settings []byte
 	termios  *unix.Termios
 }
 
-func (self *TerminalMode) Termios() (*unix.Termios, error) {
+func (self *TerminalWindow) NumberOfColumnsAndRows() (int, int, error) {
+	var winsize, err = unix.IoctlGetWinsize(unix.Stdout, unix.TIOCGWINSZ)
+	if err != nil {
+		return 0, 0, fmt.Errorf("error fetching window size: %w", err)
+	}
+
+	return int(winsize.Col), int(winsize.Row), nil
+}
+
+func (self *TerminalWindow) Termios() (*unix.Termios, error) {
 	if self.termios == nil {
 		var termios, err = unix.IoctlGetTermios(unix.Stdin, unix.TCGETS)
 		if err != nil {
@@ -25,7 +34,7 @@ func (self *TerminalMode) Termios() (*unix.Termios, error) {
 	return self.termios, nil
 }
 
-func (self *TerminalMode) Settings() ([]byte, error) {
+func (self *TerminalWindow) Settings() ([]byte, error) {
 	if self.settings == nil {
 		var termios, termiosErr = self.Termios()
 		if termiosErr != nil {
@@ -43,7 +52,7 @@ func (self *TerminalMode) Settings() ([]byte, error) {
 	return self.settings, nil
 }
 
-func (self *TerminalMode) EnableRawMode() error {
+func (self *TerminalWindow) EnableRawMode() error {
 	var termios, termiosErr = self.Termios()
 	if termiosErr != nil {
 		return termiosErr
@@ -66,7 +75,7 @@ func (self *TerminalMode) EnableRawMode() error {
 	return nil
 }
 
-func (self *TerminalMode) DisableRawMode() error {
+func (self *TerminalWindow) DisableRawMode() error {
 	var termios unix.Termios
 	var settings, settingsErr = self.Settings()
 	if settingsErr != nil {
